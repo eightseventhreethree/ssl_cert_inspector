48b61554262edeb652b0fa83a951683039526bd1
rush <eightseventhreethree>
2019-01-20 11:28:19 -0500

    modified classes to have __init__ and reworked the Check class into SSL class and defined property values with proper decorators

--- ssl_cert_inspector.py
+++ ssl_cert_inspector.py
@@ -2,70 +2,99 @@
 
 import argparse, datetime, subprocess, sys, ssl, socket, logging, pprint
 
-class Arguments(object):
-    def argumentParser(self):
+# Get CLI values and set logging level
+class CLI(object):
+    def __init__(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('-i', action='store', dest='host', help='inspect the following target to check ssl.')
+        parser.add_argument('-i', action='store', default="google.com", dest='host',
+                            help='inspect the following target to check ssl.')
         parser.add_argument('-p', action='store', default=443, dest='port', help='port that the host is listening on.')
-        parser.add_argument('-s', action='store', default='keys', dest='search', help='OpenSSL argument to look for in certificate, use "keys" to get list of keys to retrieve.')
+        parser.add_argument('-s', action='store', default='keys', dest='search',
+                            help='OpenSSL argument to look for in certificate, use "keys" to get list of keys to retrieve.')
         parser.add_argument('-v', action='store_true', default=False, dest='verbose', help='Enable verbosity/debug.')
         parsedArguments = parser.parse_args()
-        host = parsedArguments.host
-        port = int(parsedArguments.port)
-        search = parsedArguments.search
-        verbose = parsedArguments.verbose
-        if verbose:
+        self.host = parsedArguments.host
+        self.port = int(parsedArguments.port)
+        self.search = parsedArguments.search
+        self.verbose = parsedArguments.verbose
+        if self.host == None:
+            print "host undefined use -h or --help"
+            sys.exit(1)
+        if self.verbose:
             print "Verbose mode enabled."
-        return host, port, search, verbose
-
-    def logMode(self, verbose):
-        if verbose:
-            logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
             print "Logger set: DEBUG"
+            logging.basicConfig(stream=sys.stdout, level=logging.INFO)
         else:
             logging.basicConfig(stream=sys.stdout, level=logging.WARN)
 
+    @property
+    def get_host(self):
+        logging.info("get_host self.host = %s", self.host)
+        return self.host
 
-class Check(object):
-    def createConnection(self, host, port):
-        address = (host, port)
-        sslContext = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-        sslConnection = sslContext.wrap_socket(
-            socket.socket(socket.AF_INET), server_hostname=host)
-        sslConnection.settimeout(3.0)
-        sslConnection.connect((host, port))
-        logging.info("Created sslConnection.")
-        return sslConnection
+    @property
+    def get_port(self):
+        logging.info("get_port self.port =  %s", self.port)
+        return self.port
 
-    def getPeerCert(self, sslConnection):
-        peerCert = sslConnection.getpeercert(binary_form=False)
-        logging.info("Retrieved Peer Cert\n %s", peerCert)
-        return peerCert
+    @property
+    def get_search(self):
+        logging.info("get_search self.search =  %s", self.search)
+        return self.search
 
-    def parsePeerCert(self, peerCert, parseString):
+    @property
+    def get_verbose(self):
+        return self.verbose
+
+
+# Create base SSL connection to address and port
+class SSL(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self._ssl_sock = self._connector
+
+    @property
+    def _connector(self):
+        address = (self.host, self.port)
+        logging.info("_connector address = %s", address)
+        ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        ssl_builder = ssl_context.wrap_socket(
+            socket.socket(socket.AF_INET), server_hostname=self.host)
+        ssl_builder.settimeout(3.0)
+        try:
+            ssl_builder.connect(address)
+            logging.info("Created connection.")
+        except:
+            logging.error("Failed to create connection.")
+            sys.exit(1)
+        return ssl_builder
+
+    @property
+    def _set_peer_cert(self):
+        ssl_connector = self._ssl_sock
+        try:
+            peer_cert = ssl_connector.getpeercert(binary_form=False)
+            logging.info("_set_peer_cert peer_cert = %s", peer_cert)
+        except:
+            logging.error("_set_peer_cert failed to retrieve peer_cert %s", peer_cert)
+            sys.exit(1)
+        return peer_cert
+
+    def get_peer_cert_values(self, requested_value):
+        peer_cert = self._set_peer_cert
         pp = pprint.PrettyPrinter(indent=4)
-        if parseString == 'keys':
+        if requested_value == 'keys':
             print "Available keys to search for: "
-            for key, value in peerCert.iteritems():
+            for key, value in peer_cert.iteritems():
                 print " * " + key + " "
-            return parseString
+            return requested_value
         else:
-            parsedPeerCert = peerCert[parseString]
-        return parsedPeerCert
+            parsed_peer_cert = peer_cert[requested_value]
+        return parsed_peer_cert
 
-    def output(self, parsedPeerCert):
-        if parsedPeerCert == "keys":
-            quit
-        else:
-            print parsedPeerCert
 
-class Main(object):
-    def main(self):
-        host, port, search, verbose= Arguments().argumentParser()
-        Arguments().logMode(verbose)
-        sslConnecton = Check().createConnection(host, port)
-        peerCert = Check().getPeerCert(sslConnecton)
-        parsedPeerCert = Check().parsePeerCert(peerCert, search)
-        Check().output(parsedPeerCert)
-
-Main().main()
+if __name__ == '__main__':
+    cli = CLI()
+    ssl = SSL(cli.get_host, cli.get_port)
+    print ssl.get_peer_cert_values(cli.get_search)
